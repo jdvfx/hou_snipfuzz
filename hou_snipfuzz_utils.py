ac3a78d47c24dc233330333263b6bf3adac3548a
jdvfx <ju.dep@zoho.com>
2024-04-11 23:06:24 -0700

    fn to optimize hscript and reduce float precision

--- hou_snipfuzz_utils.py
+++ hou_snipfuzz_utils.py
@@ -1,3 +1,5 @@
+import re
+
 # -------------------------------------------------------------------
 
 def solversop_to_subnet(solver) -> None:
@@ -85,4 +87,61 @@ def selected_nodes_to_hscript() -> str:
     return opscript_result[0]
 
 # -------------------------------------------------------------------
+
+def reduce_float_precision(s:str,precision:int) -> str:
+
+    # TODO:
+    # use f strings {:.2f} or round()
+
+    """
+    trim decimals 
+    eg: with precision set to 2:
+     -20.149999999999999
+     -20.14
+    """
+
+    s_ =[]
+    floats = r'-*[0-9]+\.[0-9]+'
+    validchars = r'^[0-9\-\.]*$'
+
+    for i in s.split(" "):
+        if re.match(floats,i) and re.match(validchars,i):
+            k = i.split(".")
+            k[1] = k[1][:precision]
+            s = ".".join(k)
+            s = str(float(s))
+            s_.append(s)
+        else:
+            s_.append(i)
+
+    return " ".join(s_)
+
+# -------------------------------------------------------------------
+
+def optimize_hscript(hscript:str) -> str:
+
+    temp = ""
+    lines = hscript.splitlines()
+
+    for line in lines:
+        precision = 4
+        if line.startswith(("opcolor","oplocate")):
+            precision = 2
+        line = reduce_float_precision(line,precision)
+
+        """ ignore some lines """
+        comment = line.startswith("#")
+        block_divider = len(line)<2
+        wirestyle = "wirestyle" in line
+        exprlang = "opexprlanguage -s hscript" in line
+
+        if not (comment or block_divider or wirestyle or exprlang):
+            temp+=f"{line}\n"
+
+    return temp
+
+# -------------------------------------------------------------------
+
+
+
 
