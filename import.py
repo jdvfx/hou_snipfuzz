124670099d68197e8c57d361f5118eaec8e4ceee
jdvfx <ju.dep@zoho.com>
2024-04-14 23:39:13 -0700

    cleanup

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 import sys
 #import importlib
 
-sys.path.append('/home/bunker/Desktop/git/hou_snipfuzz')
+sys.path.append('/home/bunker/Desktop/git')
 import hou_snipfuzz
 
 a = hou_snipfuzz.HouSnipFuzz()
